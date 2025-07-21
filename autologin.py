9cfcf075e3f02e18097e6c2d7ac284acc541b373
E1nzbern <neteese_2009@163.com>
2025-07-21 10:49:58 +0800

    fix: 修复参数输入

--- autologin.py
+++ autologin.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import sys
+import argparse
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -8,19 +8,32 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 import logging
 
+
+parser = argparse.ArgumentParser(description="自动登录脚本")
+
+parser.add_argument("-u", "--user", required=True, help="The username to use.")
+parser.add_argument("-p", "--password", required=True, help="The password to use.")
+parser.add_argument(
+    "-d", "--driver", default="127.0.0.1:4444", help="The URL for the WebDriver."
+)
+parser.add_argument(
+    "-l",
+    "--loglevel",
+    default="INFO",
+    help="Set the logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL).",
+)
+args = parser.parse_args()
+
+user = args.user
+password = args.password
+driver_url = f"http://{args.driver}/wd/hub"
+loglevel = args.loglevel.upper()
 logging.basicConfig(
-    level=logging.INFO,  # 设置日志级别，INFO及以上级别都会被记录
+    level=loglevel,  # 设置日志级别，INFO及以上级别都会被记录
     format="%(asctime)s - %(levelname)s - %(message)s",  # 定义日志格式
     datefmt="%Y-%m-%d %H:%M:%S",  # 定义时间格式
 )
-
-if len(sys.argv) == 4:
-    user = sys.argv[1]
-    password = sys.argv[2]
-    driver_url = sys.argv[3] if len(sys.argv) > 3 else "http://127.0.0.1:4444/wd/hub"
-    logging.info(f"使用账号: {user} 进行登录")
-else:
-    quit("Usage: python autologin.py <account> <password>")
+logging.info(f"使用账号: {user} 进行登录")
 
 prefs = {"intl.accept_languages": "zh-CN,zh"}
 
@@ -40,11 +53,11 @@ options.add_argument(
 
 logging.info("脚本启动，正在初始化WebDriver...")
 try:
-    driver = webdriver.Remote(
-        command_executor=driver_url, options=options
-    )
+    driver = webdriver.Remote(command_executor=driver_url, options=options)
 except Exception as e:
-    logging.info(f"❌ 连接 WebDriver 失败，请确认 Docker 容器是否正常运行。错误: {e}")
+    logging.critical(
+        f"❌ 连接 WebDriver 失败，请确认 Docker 容器是否正常运行。错误: {e}"
+    )
     exit()
 
 try:
@@ -69,11 +82,11 @@ try:
     elif driver.title == "注销页":
         logging.info("已处于登录状态")
     else:
-        logging.info("未知状态，当前页面标题: " + driver.title)
+        logging.warning("未知状态，当前页面标题: " + driver.title)
 
 
 except Exception as e:
-    logging.info(f"执行过程中发生错误: {e}")
+    logging.critical(f"执行过程中发生错误: {e}")
 finally:
     logging.info("脚本执行完毕，关闭 WebDriver")
     driver.quit()
